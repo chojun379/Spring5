b760f64da388093febeba2dbef1fa7f369e44cb4
chojun379 <chojun379@gmail.com>
2020-11-05 22:59:45 +0900

    스프링5 프로그래밍 입문 chap07

---
+++ spring5-chap07/src/main/java/aspect2/CommonPointcut.java
@@ -0,0 +1,12 @@
+package aspect2;
+
+import org.aspectj.lang.annotation.Pointcut;
+
+public class CommonPointcut {
+	
+	@Pointcut("execution(public * chap07..*(..))")
+	public void commonTarget() {
+		
+	}
+
+}
